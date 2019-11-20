4003f23f1b145bc9d4cb003e19182605fa1fa8a3
wlawson88 <56083222+wlawson88@users.noreply.github.com>
2019-11-20 11:41:29 -0500

    updated comments on NewPing

--- newping/newping.ino
+++ newping/newping.ino
@@ -1,10 +1,19 @@
+// You put two slashes in front of whatever you want to type
+
+//Will Lawson
+//NewPing()
+//
+
+
 #include <NewPing.h>
 #define TRIGGER_PIN 6
 #define ECHO_PIN 7
 #define MAX_DISTANCE 200
 int LedPin = 13;
 int value = 0; // value is a variable that we'll use for our distance
+
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // this makes our call-sign, to get into the newping library
+
 void setup()
 {
 	pinMode(LedPin, OUTPUT);
@@ -14,22 +23,22 @@ void setup()
 void loop()
 {
 	value = sonar.ping_cm(); // it gets the distance and saves it as "value"
+	
 	delay(50);
 	Serial.println(value); // prints the distance back to you
 	delay(50);
-	// now you need to write an IF statment, and do something with it, like flash an LED
-	// or move a servo
-	// if distance goes past 10 cm turn on LED
-	if (value != 0 && value < 200)
+
+
+	if (value != 0 && value < 200)  //This ignores "bad" data
 	{
-		if (value < 10)
+		if (value < 10) //If value < 10, turn on LED, otherwise turn off
 		{
 			digitalWrite(LedPin, HIGH);
 		}
 		if (value > 10)
 			digitalWrite(LedPin, LOW);
 		{
-			analogWrite(LedPin, value * 5);
+			analogWrite(LedPin, value * 5);  // I should probably chose between this and digitalwrite(LOW)
 		}
 	}
 }
